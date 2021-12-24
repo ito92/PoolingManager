ec386be0177e95c789cf17b0083640a890b3bb0a
Miguel <mix0204@gmail.com>
2021-12-23 22:03:40 -0500

    Initial commit

---
+++ Runtime/PoolGroup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DaBois.Pooling
+{
+    public class PoolGroup<T> where T : Object
+    {
+        private T _prefab;
+        private Queue<T> _availablePool = new Queue<T>();
+        private List<T> _usedPool = new List<T>();
+
+        public PoolGroup(T prefab)
+        {
+            _prefab = prefab;
+        }
+
+        public T Get()
+        {
+            T obj;
+            if(_availablePool.Count == 0)
+            {
+                obj = Object.Instantiate(_prefab);
+            }
+            else
+            {
+                obj = _availablePool.Dequeue();
+            }
+
+            _usedPool.Add(obj);
+            return obj;
+        }
+
+        public bool Return(T obj)
+        {
+            if (_usedPool.Contains(obj))
+            {
+                _usedPool.Remove(obj);
+                _availablePool.Enqueue(obj);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Belongs(T obj)
+        {
+            return _availablePool.Contains(obj) || _usedPool.Contains(obj);
+        }
+
+        public bool IsPrefab(T obj)
+        {
+            return obj == _prefab;
+        }
+    }
+}
---
+++ Runtime/PoolingManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DaBois.Pooling
+{
+    public class PoolingManager : MonoBehaviour
+    {
+        private static PoolingManager _instance;
+        public static PoolingManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private Dictionary<Component, PoolGroup<Component>> _componentGroups = new Dictionary<Component, PoolGroup<Component>>();
+        private Dictionary<GameObject, PoolGroup<GameObject>> _gameObjectGroups = new Dictionary<GameObject, PoolGroup<GameObject>>();
+
+        private void Awake()
+        {
+            if (_instance)
+            {
+                if(_instance != this)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+            }
+
+            _instance = this;
+        }
+
+        public PoolGroup<T> CreatePool<T>(T prefab) where T : Component
+        {
+            PoolGroup<T> group = new PoolGroup<T>(prefab);
+            return group;
+        }
+
+        public T Spawn<T>(T prefab, bool activate = true) where T : Component
+        {
+            PoolGroup<Component> group;
+            if(!_componentGroups.TryGetValue(prefab, out group))
+            {
+                group = new PoolGroup<Component>(prefab);
+                _componentGroups.Add(prefab, group);
+                T pooledObj = group.Get() as T;
+                _componentGroups.Add(pooledObj, group);
+                if (activate)
+                {
+                    pooledObj.gameObject.SetActive(true);
+                }
+                return pooledObj;
+            }
+            else
+            {
+                T pooledObj = group.Get() as T;
+                if (!_componentGroups.ContainsKey(pooledObj))
+                {
+                    _componentGroups.Add(pooledObj, group);
+                }
+                if (activate)
+                {
+                    pooledObj.gameObject.SetActive(true);
+                }
+                return pooledObj;
+            }
+        }
+
+        public bool Return<T>(T obj, bool deactivate = true) where T : Component
+        {
+            PoolGroup<Component> group;
+            if (_componentGroups.TryGetValue(obj, out group))
+            {
+                bool returned = group.Return(obj);
+                if(deactivate && returned)
+                {
+                    obj.gameObject.SetActive(false);
+                }
+                return returned;
+            }
+            else
+            {
+                //print("Cannot return: " + obj);
+            }
+
+            return false;
+        }
+    }
+}
